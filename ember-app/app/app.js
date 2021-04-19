704edb633503f1b70150ac3d4dc4dd403641a12f
Billy Lando <billy_lando@yahoo.com>
2021-04-19 23:10:28 +0200

    feat: add current configs using ember-custom-elements

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -2,11 +2,16 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-app/config/environment';
+import { customElement } from 'ember-custom-elements';
 
+@customElement('ember-app-list')
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  // needed for custom elements.
+  autoboot = false;
 }
 
 loadInitializers(App, config.modulePrefix);
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <h2>Ember app should load below</h2>
+      <ember-app-list></ember-app-list>
     </div>
   );
 }
